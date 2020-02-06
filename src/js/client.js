d2c514ab6a762964ed170a089459533293eab8c4
graupv <38445832+graupv@users.noreply.github.com>
2020-02-06 16:14:27 -0600

    - Corregir append en for each - -> renderBoxContainer

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 // Reversi - Othello
 
 const APP_STATE = {
-    turn: 1,
+    turn: 1,    // 1 / -1
     /* 
     1 == p1
     2 == possible play p1
@@ -13,18 +13,44 @@ const APP_STATE = {
     board: [
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, -2, 2, 0, 0, 0],
-        [0, 0, -2, 1, -1, 2, 0, 0],
-        [0, 0, 2, -1, 1, -2, 0, 0],
-        [0, 0, 0, 2, -2, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, -1, 0, 0, 0],
+        [0, 0, 0, -1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
     ],
+
+    possiblePlays: [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, -1, 0, 0, 0],
+        [0, 0, 0, -1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    p1: 2,
+    p2: 2,
 };
 
-const checkPlays = (state) => {
-    if (state.turn == 1) {
+const checkPlays = (arr) => {
+    // 
+    let lastPos = 0;
+    let initPos = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == APP_STATE.turn) {
+            // cumple -> revisar si 
+            initPos = i;
+        };
+        
+    }
+    if () {
         // horizontal -> vertical -> diag LTR -> diag RTL
+        // find first if any, if next/prev item is not self or otherplayer
+        // can't play
+
 
     } else {
 
@@ -72,13 +98,29 @@ const renderBox = (
     
     const checker = document.createElement('div');
     checker.setAttribute('class', 'checker');
-    console.log(player, row, col);
-    
+    // console.log(player, row, col);
+    // check turn
+    if (APP_STATE.turn) {
+        // A or B
+        if (playable()) {
+
+        } else {
+            // APP_STATE.turn;
+        };
+    }
     if (player == 0) {
         checker.style.backgroundColor = 'transparent';
+        checker.onclick = () => {
+            APP_STATE.board[row][col] = 1;
+            render(root, APP_STATE);
+        };
     } else if (player == 1) {
         checker.style.backgroundColor = 'chocolate';
-        // checker.onclick = () => {console.log('lmao')};
+        checker.onclick = () => {
+            APP_STATE.board[row][col] = 1;
+            // APP_STATE.p1 += 1;
+            render(root, APP_STATE);
+        };
     } else if (player == 2) {
         checker.style.border = 'chocolate dashed';
     } else if (player == -2) {
@@ -86,6 +128,10 @@ const renderBox = (
         // checker.onclick = () => {console.log('notlmao')};
     } else {
         checker.style.backgroundColor = 'navajowhite';
+        checker.onclick = () => {
+            APP_STATE.board[row][col] = -1;
+            render(root, APP_STATE);
+        };
         // checker.onclick = () => {console.log('lmao')};
     }
 
@@ -120,10 +166,10 @@ const render = (mount, state) => {
     
     const scoreBoardP1 = document.createElement('div');
     scoreBoardP1.setAttribute('class', 'score1');
-    scoreBoardP1.innerText = 'P1 | Score: 2';
+    scoreBoardP1.innerText = 'P1 | Score: ' + APP_STATE.p1;
 
     const scoreBoardP2 = document.createElement('div');
-    scoreBoardP2.innerText = 'P2 | Score: 2';
+    scoreBoardP2.innerText = 'P2 | Score: ' + APP_STATE.p2;
     scoreBoardP2.setAttribute('class', 'score2');
 
     scoreBoard.appendChild(scoreBoardP1);
@@ -138,9 +184,7 @@ const render = (mount, state) => {
     // boxRows recibe un arreglo de contenedores de cajas con checkers
     boxRows.forEach(element => {
         contenedorBoard.appendChild(element);
-    });
-    
-    
+    }); 
 
 };
 /*
